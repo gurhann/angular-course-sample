e571cc442ed400eafe08ed8365169f2e87372f23
Gürhan Küçük <gurhan.kucuk@yandex.com>
2017-08-12 09:42:39 +0300

    basic assignment 1.

---
+++ basics-assignment-1-start/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success_alert/success_alert.component'; 
+import { WarningAlertComponent } from './warning_alert/warning_alert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ basics-assignment-1-start/src/app/warning_alert/warning_alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'warning-alert',
+    styleUrls: ['./warning_alert.component.css'],
+    templateUrl: './warning_alert.component.html'
+})
+export class WarningAlertComponent {
+
+}
